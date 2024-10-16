ca91296818f0c1a55a55155326ef0e7322d2f40a
xfo79k <xfo79k@gmail.com>
2024-10-17 00:05:41 +0800

    feat: update antd

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,51 @@
-import { ThemeProvider } from '@/provider/theme-provider.tsx';
-import { RouterProvider } from 'react-router-dom';
-import router from '@/routes.tsx';
+import { App as AntdApp, Avatar, Button, ConfigProvider, Tag, theme } from 'antd';
+
+export const Search = () => {
+  return (
+    <div className='h-16 flex justify-between items-center gap-3 px-3 border-b border-b-gray-100/80'>
+      <div className='relative cursor-pointer'>
+        <Avatar
+          size={42}
+          shape='square'
+          className='bg-zinc-100'
+          icon={<span className='icon-[token-branded--nav]' />}
+        >
+          UL
+        </Avatar>
+      </div>
+      <div className='flex-1 h-full'>
+        <input
+          placeholder='Hi Ulify'
+          className='outline-none h-full w-full bg-transparent font-light text-xl'
+        />
+      </div>
+      <Tag className='opacity-60'>Esc</Tag>
+    </div>
+  );
+};
+
+
+export const Result = ()=>{
+  return (
+    <div>123</div>
+  )
+}
+
+export const ContentWrapper = ()=>{
+  return (
+    <div>
+
+    </div>
+  )
+}
 
 function App() {
   return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <ConfigProvider theme={{ cssVar: true }} prefixCls='u'>
+      <AntdApp className='app-container'>
+        <Search />
+      </AntdApp>
+    </ConfigProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 import { addDynamicIconSelectors } from '@iconify/tailwind';
-import * as tailwindcssAnimate from 'tailwindcss-animate';
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
-  darkMode: ['class'],
-  content: ['./src/**/*.{ts,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    extend: {
+      colors: {
+        primary: '#1677ff',
+        success:'#52c41a',
+        warning:'#faad14',
+        error:'#ff4d4f',
+        gray:{
+          50:'#fafafa',
+          100:'#f5f5f5',
+          200:'#d9d9d9',
+          300:'#bfbfbf',
+          400:'#8c8c8c',
+          500:'#595959',
+          600:'#434343',
+          700:'#262626',
+          800:'#1f1f1f',
+          900:'#141414',
+          950:'#000000'
+        }
+      }
+    }
   },
-  plugins: [tailwindcssAnimate,addDynamicIconSelectors()],
-});
+  plugins: [addDynamicIconSelectors()]
+};
